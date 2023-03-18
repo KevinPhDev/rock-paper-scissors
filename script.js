1c8963421f720ddca4d483316c5d313f8bc67fd1
KevinPhDev <kevinphdev@outlook.com>
2023-03-18 18:17:37 +1100

    Create function to randomly return 'rock', 'paper' or 'scissors'

---
+++ script.js
@@ -0,0 +1,6 @@
+function getComputerChoice() {
+    let randomNumber = Math.floor((Math.random() * 3) + 1)
+    if (randomNumber === 1) return "Rock";
+    if (randomNumber === 2) return "Paper";
+    if (randomNumber === 3) return "Scissors";
+}
