0181a479e4fa1bb6e7c5e6c859fdce355c21245b
KevinPhDev <kevinphdev@outlook.com>
2023-03-18 21:46:01 +1100

    Made playSelection() prompt input case-insensitive

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 function getComputerChoice() {
     let randomNumber = Math.floor((Math.random() * 3) + 1)
-    if (randomNumber === 1) return "Rock";
-    if (randomNumber === 2) return "Paper";
-    if (randomNumber === 3) return "Scissors";
+    if (randomNumber === 1) return "ROCK";
+    if (randomNumber === 2) return "PAPER";
+    if (randomNumber === 3) return "SCISSORS";
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "Rock" && computerSelection === "Paper")
-        return "You lose, paper beats rock";
-    if (playerSelection === "Rock" && computerSelection === "Scissors")
-        return "You win, rock beats scissors";
-    if (playerSelection === "Paper" && computerSelection === "Rock")
-        return "You win, paper beats rock";
-    if (playerSelection === "Paper" && computerSelection === "Scissors")
-        return "You lose, scissors beats paper";
-    if (playerSelection === "Scissors" && computerSelection === "Rock")
-        return "You lose, rock beats scissors";
-    if (playerSelection === "Scissors" && computerSelection === "Paper")
-        return "You win, scissors beats paper";
+    if (playerSelection === "ROCK" && computerSelection === "PAPER")
+        return "You Lose! Paper beats Rock";
+    if (playerSelection === "ROCK" && computerSelection === "SCISSORS")
+        return "You Win! Rock beats Scissors";
+    if (playerSelection === "PAPER" && computerSelection === "ROCK")
+        return "You Win! Paper beats Rock";
+    if (playerSelection === "PAPER" && computerSelection === "SCISSORS")
+        return "You Lose! Scissors beats Paper";
+    if (playerSelection === "SCISSORS" && computerSelection === "ROCK")
+        return "You Lose! Rock beats Scissors";
+    if (playerSelection === "SCISSORS" && computerSelection === "PAPER")
+        return "You Win! Scissors beats Paper";
     if (playerSelection === computerSelection)
         return "It's a draw";
 }
 
-const playerSelection;
+const playerSelection = prompt("Enter Rock, Paper or Scissors").toUpperCase();
 const computerSelection = getComputerChoice();
+
+
+
