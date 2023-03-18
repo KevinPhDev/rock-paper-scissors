42020436cbd10fde4ae5901629d8ce42bac89794
KevinPhDev <kevinphdev@outlook.com>
2023-03-18 18:41:24 +1100

    Create function playRound() to return match results

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function getComputerChoice() {
     if (randomNumber === 2) return "Paper";
     if (randomNumber === 3) return "Scissors";
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "Paper")
+        return "You lose, paper beats rock";
+    if (playerSelection === "Rock" && computerSelection === "Scissors")
+        return "You win, rock beats scissors";
+    if (playerSelection === "Paper" && computerSelection === "Rock")
+        return "You win, paper beats rock";
+    if (playerSelection === "Paper" && computerSelection === "Scissors")
+        return "You lose, scissors beats paper";
+    if (playerSelection === "Scissors" && computerSelection === "Rock")
+        return "You lose, rock beats scissors";
+    if (playerSelection === "Scissors" && computerSelection === "Paper")
+        return "You win, scissors beats paper";
+    if (playerSelection === computerSelection)
+        return "It's a draw";
+}
+
+const playerSelection;
+const computerSelection = getComputerChoice();
