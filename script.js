4358c5fcbc2e09cc10325e16d5d89c0a11307d2a
KevinPhDev <kevinphdev@outlook.com>
2023-03-25 14:04:00 +1100

    Recreate game() function to sync with buttons and score tab

--- script.js
+++ script.js
@@ -22,50 +22,61 @@ function getComputerChoice() {
 
 function playRound() {
     if (playerSelection === "ROCK" && computerSelection === "PAPER") {
-        computerScore++;
         roundResult.textContent = `You Lose! Paper beats Rock!`;
+        return "Lose";
     } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
-        playerScore++;
         roundResult.textContent = `You Win! Rock beats Scissors!`;
+        return "Win";
     } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
-        playerScore++;
         roundResult.textContent = `You Win! Paper beats Rock!`;
+        return "Win";
     } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-        computerScore++;
         roundResult.textContent = `You Lose! Scissors beats Paper!`;
+        return "Lose";
     } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
-        computerScore++;
         roundResult.textContent = `You Lose! Rock beats Scissors!`;
+        return "Lose";
     } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
-        playerScore++;
         roundResult.textContent = `You Win! Scissors beats Paper!`;
+        return "Win";
     } else if (playerSelection === computerSelection) {
         roundResult.textContent = `It's a Draw!`;
+        return "Draw";
     }
+}
+
+function game() {
+    if (playRound() === "Win") {
+        playerScore++;
+    } else if (playRound() === "Lose") {
+        computerScore++;
+    }
+    scoreTab.textContent = `Player: ${playerScore} Computer: ${computerScore}`
 }
 
 function resetGame() {
     roundResult.textContent = 'Start Game';
     playerScore = 0;
     computerScore = 0;
+    scoreTab.textContent = `Player: ${playerScore} Computer: ${computerScore}`
 }
 
 rock.addEventListener('click', function(event) {
     playerSelection = "ROCK";
     computerSelection = getComputerChoice();
-    playRound(playerSelection, computerSelection);
+    game();
 })
 
 paper.addEventListener('click', function(event) {
     playerSelection = "PAPER";
     computerSelection = getComputerChoice();
-    playRound(playerSelection, computerSelection);
+    game();
 })
 
 scissors.addEventListener('click', function(event) {
     playerSelection = "SCISSORS";
     computerSelection = getComputerChoice();
-    playRound(playerSelection, computerSelection);
+    game();
 })
 
 resetButton.addEventListener('click', function(event) {
